d4f88b4f99caa9952cad14247bc54718fafc29fa
cvalencia-godaddy <cvalencia@godaddy.com>
2022-08-19 17:15:33 -0500

    adding context to -app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,25 @@
+import { createContext } from "react";
 import "../styles/globals.css";
+
+const StoreContext = createContext();
+const StoreProvider = ({ children }) => {
+  const initialState = {
+    latLong: "",
+    coffeeStores: [],
+  };
+  return (
+    <StoreContext.Provider value={{ state: initialState }}>
+      {children}
+    </StoreContext.Provider>
+  );
+};
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Component {...pageProps} />
+      <StoreProvider>
+        <Component {...pageProps} />
+      </StoreProvider>
     </>
   );
 }
